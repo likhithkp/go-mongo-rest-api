e2fd54d28313cad235d99939152e35b1ba8e75fe
likhithkp <likhithkpdev@gmail.com>
2024-05-27 07:10:53 +0530

    Mongo integrated

---
+++ controllers/user.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"encoding/json"
+	"go-mongo-rest-api/models"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserController struct {
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+func (uc *UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+	u := models.User{}
+	if err := uc.Session.DB("mongo-golang").C("users").FindId(oid).One(&u); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+	if err! =nil {
+		fmt.Printlnerr
+	}
+
+
+}
+
+// CreateUser
+// DeleteUser
--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"go-mongo-rest-api/controllers"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
-	fmt.Println("Hello")
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user/", uc.CreateUser)
+	r.DELETE("user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
